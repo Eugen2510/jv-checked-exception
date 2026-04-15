7a01aa6a2f0da451a07e9400e471ec1224688c6e
Eugene L <lugovoj20@gmail.com>
2026-04-15 12:28:19 +0200

    Fixed checkstyle warnings

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,8 +1,12 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password == null || password.length() < 10 || !password.equals(repeatPassword)) {
+    private static final int MIN_PASSWORD_LENGTH = 10;
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null
+                || password.length() < MIN_PASSWORD_LENGTH
+                || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
